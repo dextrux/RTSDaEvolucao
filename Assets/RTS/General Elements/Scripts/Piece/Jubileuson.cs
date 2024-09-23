23c51fb197c2736516cbb55f48d8c68e16785cbe
Arthur Roque <arthur.roque.pro@gmail.com>
2024-09-23 11:43:41 -0300

    Adição da primeira fase da construção do Jubileuson

--- Assets/RTS/General Elements/Scripts/Piece/Jubileuson.cs
+++ Assets/RTS/General Elements/Scripts/Piece/Jubileuson.cs
@@ -4,5 +4,59 @@ using UnityEngine;
 
 public class Jubileuson : MonoBehaviour
 {
+    // Atributos da classe
+    private Temperature _pieceTemperature;
+    private Humidity _pieceHumidity;
+    private StatusBar _healthBar;
+    private StatusBar _energyBar;
+    private StatusBar _fertilityBar;
+    private StatusBar _dietTendency;
+    private StatusBar _strengthBar;
+    private PieceDiet _pieceDiet;
+    private PieceLevel _pieceLevel;
+    private int _pieceRemainingActions;
 
+    // Métodos
+    public void InitializePiece(Temperature temperature, Humidity humidity, StatusBar healthBar, StatusBar energyBar, StatusBar fertilityBar, StatusBar dietTendency, StatusBar strengthBar, PieceDiet pieceDiet, PieceLevel pieceLevel)
+    {
+        _pieceTemperature = temperature;
+        _pieceHumidity = humidity;
+        _healthBar = healthBar;
+        _energyBar = energyBar;
+        _fertilityBar = fertilityBar;
+        _dietTendency = dietTendency;
+        _strengthBar = strengthBar;
+        _pieceDiet = pieceDiet;
+        _pieceLevel = pieceLevel;
+        ResetActions();
+    }
+
+    public void ResetActions()
+    {
+        _pieceRemainingActions = (int)_pieceLevel; // Reset to default actions
+    }
+
+    // Getters
+    public Temperature GetTemperature() => _pieceTemperature;
+    public Humidity GetHumidity() => _pieceHumidity;
+    public StatusBar GetHealthBar() => _healthBar;
+    public StatusBar GetEnergyBar() => _energyBar;
+    public StatusBar GetFertilityBar() => _fertilityBar;
+    public StatusBar GetDietTendency() => _dietTendency;
+    public StatusBar GetStrengthBar() => _strengthBar;
+    public PieceDiet GetDiet() => _pieceDiet;
+    public PieceLevel GetLevel() => _pieceLevel;
+    public int GetRemainingActions() => _pieceRemainingActions;
+
+    // Setters
+    public void SetTemperature(Temperature temperature) => _pieceTemperature = temperature;
+    public void SetHumidity(Humidity humidity) => _pieceHumidity = humidity;
+    public void SetHealthBar(StatusBar healthBar) => _healthBar = healthBar;
+    public void SetEnergyBar(StatusBar energyBar) => _energyBar = energyBar;
+    public void SetFertilityBar(StatusBar fertilityBar) => _fertilityBar = fertilityBar;
+    public void SetDietTendency(StatusBar dietTendency) => _dietTendency = dietTendency;
+    public void SetStrengthBar(StatusBar strengthBar) => _strengthBar = strengthBar;
+    public void SetDiet(PieceDiet pieceDiet) => _pieceDiet = pieceDiet;
+    public void SetLevel(PieceLevel pieceLevel) => _pieceLevel = pieceLevel;
 }
+
---
+++ Assets/RTS/General Elements/Scripts/Piece/StatusBar.cs
@@ -0,0 +1,39 @@
+﻿public class StatusBar
+{
+    // Constantes da classe
+    float _maxBarValue = 100f;
+    const float _minBarValue = 0f;
+
+    // Atributo
+    private float _currentValue;
+
+    // Boundary Check
+    private float AdjustToClosestBoundary(float value)
+    {
+        if (value > _maxBarValue)
+        {
+            return _maxBarValue;
+        }
+        else if (value < _minBarValue)
+        {
+            return _minBarValue;
+        }
+        return value;
+    }
+
+    // Get's
+    public float GetCurrentBarValue() { return _currentValue; }
+    public float GetBarMaxValue() { return _maxBarValue; }
+    public float GetBarMinValue() { return _minBarValue; }
+
+    // String Get's
+    public string GetCurrentBarValueAsString() { return _currentValue.ToString(); }
+    public string GetBarMaxValueAsString() { return _maxBarValue.ToString(); }
+    public string GetBarMinValueAsString() { return _minBarValue.ToString(); }
+
+    // Set's
+    public void SetNewBarValue(float value) { _currentValue = AdjustToClosestBoundary(value); }
+    // Set new Max Value
+    public void SetNewMaxBoundarie(float value){ _maxBarValue = value; }
+}
+
