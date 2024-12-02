72582cca4110c51280aff88e1d4344d208d4afcf
dextrux <titolucas1@live.com>
2024-12-01 21:32:50 -0300

    Fix

--- Assets/Mutation/Scripts/Tile/TileVisualSet.cs
+++ Assets/Mutation/Scripts/Tile/TileVisualSet.cs
@@ -16,8 +16,12 @@ public class TileVisualSet : MonoBehaviour
         for (int i = 0; i < _spawnPoints.Length; i++)
         {
             Debug.Log(TileVisualSingleton.Instance.ObjetoParaInstanciar(tile.biome));
-            GameObject newObj = Instantiate(TileVisualSingleton.Instance.ObjetoParaInstanciar(tile.biome), _spawnPoints[i].position, transform.rotation);
+            GameObject newObj = Instantiate(TileVisualSingleton.Instance.ObjetoParaInstanciar(tile.biome), _spawnPoints[i].position, Quaternion.Euler(transform.rotation.x, Random.Range(0, 360), transform.rotation.z));
             _activeObjectTiles[i] = newObj;
+        }
+        foreach (GameObject obj in _activeObjectTiles)
+        {
+            obj.transform.localScale = new Vector3(Random.Range(0.8f, 1.2f), Random.Range(0.8f, 1.2f), Random.Range(0.8f, 1.2f));
         }
     }
     private void DespawnObjects()
